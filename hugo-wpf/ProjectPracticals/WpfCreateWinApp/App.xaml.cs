8f0f49309fb4ac262ad21de6e3b945d1984cabf2
Hugo MG Sung <personar95@naver.com>
2025-06-01 17:57:43 +0900

    모냐

---
+++ hugo-wpf/ProjectPracticals/WpfCreateWinApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using WpfCreateWinApp.Views;
+
+namespace WpfCreateWinApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var view = new MainView();
+            view.Show();
+        }
+    }
+
+}
---
+++ hugo-wpf/ProjectPracticals/WpfCreateWinApp/Views/MainView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfCreateWinApp.Views
+{
+    /// <summary>
+    /// MainView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            var view = new SubView();
+
+            switch (CboWindowStyle.SelectedIndex)
+            {
+                case 0:
+                    view.WindowStyle = WindowStyle.None;
+                    break;
+                case 1:
+                    view.WindowStyle = WindowStyle.SingleBorderWindow;
+                    break;
+                case 2:
+                    view.WindowStyle = WindowStyle.ThreeDBorderWindow;
+                    break;
+                case 3:
+                    view.WindowStyle = WindowStyle.ToolWindow;
+                    break;
+            };
+
+            switch (CboStartLocation.SelectedIndex)
+            {
+                case 0:
+                    view.WindowStartupLocation = WindowStartupLocation.Manual;
+                    break;
+                case 1:
+                    view.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                    break;
+                case 2:
+                    view.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                    break;
+            };
+
+            view.Owner = this;
+
+            switch (CboModalType.SelectedIndex)
+            {
+                case 0:
+                    view.ShowDialog();
+                    break;
+                case 1:
+                    view.Show();
+                    break;
+            }
+        }
+    }
+}
